c6af9039de51b3d8a22b4365c7b41d0f4aa1eef4
Ekkehard <emkinter@me.com>
2020-12-29 09:22:04 -0700

    Error Fixes

--- Package.swift
+++ Package.swift
@@ -1,9 +1,11 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "CommandLineTool",
     dependencies: [
         .package(
+            name: "Files",
             url: "https://github.com/johnsundell/files.git",
             from: "4.0.0"
         )
@@ -16,7 +18,7 @@ let package = Package(
         .target(
             name: "CommandLineToolCore",
             dependencies: ["Files"]
-        )
+        ),
         .testTarget(
             name: "CommandLineToolTests",
             dependencies: ["CommandLineToolCore", "Files"]
